d36e2346d0229d6545c759dcd9909f6ccdb7aecd
Nicholas Greschuk <nicholas.greschuk@gmail.com>
2023-05-28 01:13:03 -0500

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import {
-  withAuthenticator,
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
@@ -27,6 +28,15 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   };
 
@@ -36,7 +46,9 @@ const App = ({ signOut }) => {
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -45,9 +57,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   };
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -95,12 +108,25 @@ const App = ({ signOut }) => {
             <Text as="span">
               {note.description}
             </Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
           </Flex>
         ))}
       </View>
+      <View 
+        name="image"
+        as="input"
+        type="file"
+        style={{ alignSelf: "end" }}
+      />
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
