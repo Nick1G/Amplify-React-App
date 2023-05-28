d3b20f08d45bf7b5b2c41b073bc7644e932f1bb3
Nicholas Greschuk <nicholas.greschuk@gmail.com>
2023-05-27 23:26:44 -0500

    added create and delete notes using graphql api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactapp27290e01": {
       "AppClientID": "string",
